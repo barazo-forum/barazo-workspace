44c7a82b79a7242ee2b7c062e51d443f1c018453
Guido X Jansen <x@gui.do>
2026-02-20 23:45:43 +0100

    chore(workspace): unify DX configs and fix lockfile drift (#41)
    - Update catalog: typescript-eslint ^8.55.0 -> ^8.56.0
    - Add type: module to root package.json (fixes ESM config warnings)
    - Unify commitlint: add a11y + security types, subject-case: [0]
    - Remove empty plugins array from prettier config
    - Regenerate lockfile to resolve drift

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,30 @@
 export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "a11y",
+        "security",
+      ],
+    ],
     "scope-enum": [
       2,
       "always",
       ["lexicons", "api", "web", "deploy", "workspace"],
     ],
+    "subject-case": [0],
   },
 };
